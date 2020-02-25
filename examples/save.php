105ed9029cf6be06ce6e9f501479bd9335b3799b
Gen Taliaru <gen@infira.ee>
2020-02-25 21:01:22 +0200

    do not use header in save

--- examples/save.php
+++ examples/save.php
@@ -2,6 +2,4 @@
 require_once "../PDFCreatorAPI.class.php";
 
 $Pdf = new \Infira\PDFCreatorAPI('serverUrl', 'apiKey');
-header("Content-type: application/pdf");
-header('Pragma: no-cache');
 file_put_contents("test.pdf", $Pdf->convertHTML("tere"));
